7d04e9de391bb179b6dc68dcd3afc6f10ef12900
Gabriel <gabrivalle8@gmail.com>
2024-04-30 21:22:09 -0300

    fazendo barra de pesquisa para navegar pelo o mapa

--- SENAI/PPDM/Aulas/aula06/app-maps/App.js
+++ SENAI/PPDM/Aulas/aula06/app-maps/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import MapView, { Marker } from 'react-native-maps';
-import { View } from 'react-native';
+import { TextInput, TouchableOpacity, View } from 'react-native';
+import { FontAwesome6 } from '@expo/vector-icons';
 
 import {
   requestForegroundPermissionsAsync, // Solicita ao usuário a permissão para utilizar a localização
   getCurrentPositionAsync, // Quando autorizado nos retorna a posição do usuário
   watchPositionAsync, // Observa mudança na localização da posição
   LocationAccuracy,
+  geocodeAsync,
 
 } from 'expo-location';
 import { styles } from "./src/styles/styles";
@@ -14,6 +16,7 @@ import { styles } from "./src/styles/styles";
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  const [searchAddress, setSearchAddress] = useState('');
 
   const mapRef = useRef(null);
 
@@ -24,7 +27,14 @@ export default function App() {
 
     if (granted) {
       const currentPosition = await getCurrentPositionAsync();
-      setLocation(currentPosition)
+      const { coords: { latitude, longitude } } = currentPosition
+      setLocation({ latitude, longitude });
+
+      mapRef.current?.animateCamera({
+        pitch: 70,
+        center: { latitude, longitude }
+      })
+
       // Exibindo no console as informações da posição atual
       console.log(`LOCALIZAÇÃO ATUAL => `, currentPosition);
     }
@@ -33,6 +43,34 @@ export default function App() {
   useEffect(() => {
     requestLocationPermissions()
   }, []);
+
+
+
+
+
+  const searchCoordinates = async (address) => {
+    try {
+      const result = await geocodeAsync(address);
+      console.log('result', result);
+      if (result && result.length > 0) {
+        const { latitude, longitude } = result[0];
+
+        setLocation({ latitude, longitude });
+
+        mapRef.current?.animateCamera({
+          pitch: 70,
+          center: { latitude, longitude }
+        })
+      } else {
+        console.log('Endereço não encontrado');
+      }
+    } catch (error) {
+      console.log('Erro ao converte endereço:', error);
+    }
+  };
+
+
+
 
   return (
     <View style={styles.container}>
@@ -43,20 +81,37 @@ export default function App() {
           ref={mapRef}
           style={styles.map}
           initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
+            latitude: location.latitude,
+            longitude: location.longitude,
             latitudeDelta: 0.005,
             longitudeDelta: 0.005
           }}
         >
           <Marker
             coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude
+              latitude: location.latitude,
+              longitude: location.longitude
             }}
           />
         </MapView>
       }
+      <View style={styles.searchContainer}>
+
+        <View style={styles.searchInput}>
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="Digite o endereço"
+            value={searchAddress}
+            onChangeText={(text) => setSearchAddress(text)}
+          />
+          <TouchableOpacity style={styles.searchButton} onPress={() => searchCoordinates(searchAddress)}>
+            <FontAwesome6 name='magnifying-glass' size={26} color='#BDC9DE' />
+
+          </TouchableOpacity>
+
+        </View>
+
+      </View>
     </View>
   );
 }
--- SENAI/PPDM/Aulas/aula06/app-maps/src/styles/styles.js
+++ SENAI/PPDM/Aulas/aula06/app-maps/src/styles/styles.js
@@ -1,16 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight} from 'react-native-status-bar-height'
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-        flex: 1,
-        width: '100%'
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+    width: '100%'
+  },
+  searchContainer: {
+    position: 'absolute',
+    top: Platform.OS === 'android' ? getStatusBarHeight() : 0,
+    left: 10,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+  },
+  searchInput: {
+    width: '83%',
+    flexDirection: 'row',
+    height: 40,
+    borderColor: '#fff',
+    borderRadius: 10,
+    backgroundColor: 'white',
+    elevation: 10,
+    padding: 5,
+    paddingRight: 40,
+    marginTop: 40,
+    marginLeft: 40
+  },
+  searchButton: {
+    padding: 7,
+    position: 'absolute',
+    right: 0,
+    transform: [{ rotate: '90deg' }]
+  }
+});
